baf0bfb22e2222b97ec0c845800def16ea188069
EasyGameProduction <sebastianpfuelb@gmail.com>
2023-11-17 16:57:52 +0100

    ErsterHydrant

--- src/views/HydrantenView.vue
+++ src/views/HydrantenView.vue
@@ -1,56 +1,81 @@
 <template>
-    <Header ueberschrift="Hydranten"/>
-    <div id="hydranten">
-        <l-map ref="map" v-model:zoom="zoom" :center="[this.lat, this.long]">
-            <l-tile-layer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            layer-type="base"
-            name="OpenStreetMap"
-        ></l-tile-layer>
-        </l-map>
-    </div>
-    <Footer backLink="/einsatz"/>
+  <Header ueberschrift="Hydranten" />
+  <div id="hydranten">
+    <l-map ref="map" v-model:zoom="zoom" :center="[this.lat, this.long]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+      <l-marker :lat-lng="this.markerPosition">
+        <l-icon>
+            <img class="iconImg" src="../assets/KartenIcons/Hydrant.svg">
+        </l-icon>
+      </l-marker>
+    </l-map>
+  </div>
+  <Footer backLink="/einsatz" />
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet"
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import { LMap, LTileLayer, LMarker, LIcon } from "@vue-leaflet/vue-leaflet";
 
 export default {
-    name: 'HydrantenView',
-    components: {
-        Header,
-        Footer,
-        LMap,
-        LTileLayer
-    },
-    data(){
-        return{
-            zoom: 50,
-            lat: 50.104040,
-            long: 10.143591,
-        }
-    },
-    created(){
-        const success = (position) => {
-            this.lat = position.coords.latitude;
-            this.long = position.coords.longitude;
-        };
+  name: "HydrantenView",
+  components: {
+    Header,
+    Footer,
+    LMap,
+    LTileLayer,
+    LMarker,
+    LIcon
+  },
+  data() {
+    return {
+      zoom: 50,
+      lat: 50.10404,
+      long: 10.143591,
+      markerPosition: [50.104254, 10.143308],
+      iconText: "400",
+    };
+  },
+  methods: {
+    setGPSPosition: function () {
+      const success = (position) => {
+        this.lat = position.coords.latitude;
+        this.long = position.coords.longitude;
+      };
 
-        const error = (err) => {
-            console.log(error);
-        }
+      const error = (err) => {
+        console.log(error);
+      };
 
-        navigator.geolocation.getCurrentPosition(success, error);
+      navigator.geolocation.getCurrentPosition(success, error);
+    },
+    setHydrant: function (lat, long, durchlaufmenge, bemerkung) {
+
     }
-}
+  },
+  created() {
+    this.setGPSPosition();
+  }
+};
 </script>
 
 <style lang="scss">
-#hydranten{
-    margin-top: 4.5rem;
-    width: 100%;
-    height: 72vh;
+#hydranten {
+  margin-top: 4.5rem;
+  width: 100%;
+  height: 72vh;
+
+  .iconImg{
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: -100%;
+    max-height: 2.5rem !important;
+  }
 }
 </style>
