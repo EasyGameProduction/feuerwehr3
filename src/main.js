11bfdf4cf6da3eed369ec609043ac7550f91fc2a
EasyGameProduction <sebastianpfuelb@gmail.com>
2023-11-17 15:57:51 +0100

    hydrantenMap Initialisation

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
+import 'leaflet/dist/leaflet.css'
 
 createApp(App).use(VueSweetalert2).use(store).use(router).mount('#app')
--- src/views/HydrantenView.vue
+++ src/views/HydrantenView.vue
@@ -1,7 +1,13 @@
 <template>
     <Header ueberschrift="Hydranten"/>
     <div id="hydranten">
-        <iframe src="https://www.osmhydrant.org/de/" title="Hydrantenmap"></iframe>
+        <l-map ref="map" v-model:zoom="zoom" :center="[this.lat, this.long]">
+            <l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            layer-type="base"
+            name="OpenStreetMap"
+        ></l-tile-layer>
+        </l-map>
     </div>
     <Footer backLink="/einsatz"/>
 </template>
@@ -9,23 +15,42 @@
 <script>
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet"
 
 export default {
     name: 'HydrantenView',
     components: {
         Header,
-        Footer
+        Footer,
+        LMap,
+        LTileLayer
+    },
+    data(){
+        return{
+            zoom: 50,
+            lat: 0,
+            long: 0,
+        }
+    },
+    created(){
+        const success = (position) => {
+            this.lat = position.coords.latitude;
+            this.long = position.coords.longitude;
+        };
+
+        const error = (err) => {
+            console.log(error);
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error);
     }
 }
 </script>
 
 <style lang="scss">
 #hydranten{
-    iframe{
-        width: 100%;
-        height: 100vh;
-        margin: 0;
-        padding: 0;
-    }
+    margin-top: 4.5rem;
+    width: 100%;
+    height: 72vh;
 }
 </style>
