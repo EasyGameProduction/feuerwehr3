3460db7327af9e5c837245299beb79d3cc4d08de
EasyGameProduction <sebastianpfuelb@gmail.com>
2023-11-28 19:10:08 +0100

    Weitere Hydranten

--- src/store/index.js
+++ src/store/index.js
@@ -53,11 +53,155 @@ export default createStore({
           bemerkung: ""
         },
         {
-          id: 6,
+          id: 7,
           art: "Unterflurhydrant",
           lat: "50.101910",
           lng: "10.142975",
           durchlaufmenge: "100 l/min",
+          bemerkung: ""
+        },
+        {
+          id: 8,
+          art: "Unterflurhydrant",
+          lat: "50.103213",
+          lng: "10.144095",
+          durchlaufmenge: "80 l/min",
+          bemerkung: ""
+        },
+        {
+          id: 9,
+          art: "Unterflurhydrant",
+          lat: "50.102092",
+          lng: "10.144803",
+          durchlaufmenge: "80 l/min",
+          bemerkung: ""
+        },
+        {
+          id: 10,
+          art: "Unterflurhydrant",
+          lat: "50.10135",
+          lng: "10.14523",
+          durchlaufmenge: "80 l/min",
+          bemerkung: "Auf der Straße rechts vorm Feldweg"
+        },
+        {
+          id: 11,
+          art: "Überflurhydrant",
+          lat: "50.10046",
+          lng: "10.14563",
+          durchlaufmenge: "80 l/min",
+          bemerkung: "Links von der Pfarrheim Einfahrt"
+        },
+        {
+          id: 12,
+          art: "Unterflurhydrant",
+          lat: "50.10150",
+          lng: "10.14330",
+          durchlaufmenge: "125 l/min",
+          bemerkung: "Gegenüber der Stern-Einfahrt am Bordsteinrand"
+        },
+        {
+          id: 13,
+          art: "Unterflurhydrant",
+          lat: "50.10138",
+          lng: "10.14432",
+          durchlaufmenge: "80 l/min",
+          bemerkung: "Position ungenau!"
+        },
+        {
+          id: 14,
+          art: "Unterflurhydrant",
+          lat: "50.100937",
+          lng: "10.144393",
+          durchlaufmenge: "80 l/min",
+          bemerkung: "In der Seitenstraße"
+        },
+        {
+          id: 15,
+          art: "Überflurhydrant",
+          lat: "50.100384",
+          lng: "10.144500",
+          durchlaufmenge: "125 l/min",
+          bemerkung: "Links neben Hauswand"
+        },
+        {
+          id: 16,
+          art: "Unterflurhydrant",
+          lat: "50.100027",
+          lng: "10.145201",
+          durchlaufmenge: "100 l/min",
+          bemerkung: ""
+        },
+        {
+          id: 17,
+          art: "Unterflurhydrant",
+          lat: "50.099884",
+          lng: "10.146112",
+          durchlaufmenge: "100 l/min",
+          bemerkung: ""
+        },
+        {
+          id: 18,
+          art: "Unterflurhydrant",
+          lat: "50.099666",
+          lng: "10.144080",
+          durchlaufmenge: "80 l/min",
+          bemerkung: "Auf dem Parkplatz"
+        },
+        {
+          id: 19,
+          art: "Unterflurhydrant",
+          lat: "50.099298",
+          lng: "10.142934",
+          durchlaufmenge: "200 l/min",
+          bemerkung: ""
+        },
+        {
+          id: 20,
+          art: "Unterflurhydrant",
+          lat: "50.099221",
+          lng: "10.141542",
+          durchlaufmenge: "100 l/min",
+          bemerkung: "Auf dem Gehweg"
+        },
+        {
+          id: 21,
+          art: "Unterflurhydrant",
+          lat: "50.09975690631743",
+          lng: "10.140750515229318",
+          durchlaufmenge: "100 l/min",
+          bemerkung: "Auf dem Gehweg"
+        },
+        {
+          id: 22,
+          art: "Unterflurhydrant",
+          lat: "50.098783",
+          lng: "10.143942",
+          durchlaufmenge: "150 l/min",
+          bemerkung: ""
+        },
+        {
+          id: 23,
+          art: "Unterflurhydrant",
+          lat: "50.097921",
+          lng: "10.143825",
+          durchlaufmenge: "150 l/min",
+          bemerkung: ""
+        },
+        {
+          id: 24,
+          art: "Unterflurhydrant",
+          lat: "50.097119",
+          lng: "10.143716",
+          durchlaufmenge: "150 l/min",
+          bemerkung: ""
+        },
+        {
+          id: 25,
+          art: "Unterflurhydrant",
+          lat: "50.096162",
+          lng: "10.143590",
+          durchlaufmenge: "150 l/min",
           bemerkung: ""
         },
         {
